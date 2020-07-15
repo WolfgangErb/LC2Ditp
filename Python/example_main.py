d05bb1517ccb1a4f8066605e2584f46a40a60159
WolfgangErb <erb@math.hawaii.edu>
2020-07-15 12:53:36 +0100

    Update filename

--- Python/example_main.py
+++ Python/example_main.py
@@ -8,7 +8,7 @@ This python implementation was written by W. Erb (01.10.2018)
 """
 
 import LC2Ditp as LC
-from mpl_toolkits import mplot3d
+#from mpl_toolkits import mplot3d
 import numpy as np
 from numpy import linalg as LA
 import matplotlib.pyplot as plt
@@ -18,13 +18,13 @@ LCrange = [0, 1, 0, 1]       #Range of Lissajous curves
 m       = [28,29]            #Frequency parameters
 kappa   = [0,0]              #Phase shift parameter
 
-nofun   = 3                  #Number of test function
+nofun   = 2                  #Number of test function
 average = 0                  #Averaging on boundaries
 Nd      = 100                #Discretization for plot
 
 # Extract LC points
 xLC, yLC, wLC = LC.LC2Dpts(m,kappa,LCrange)
-NoLC = np.count_nonzero(xLC)
+NoLC = wLC.size
 
 # Extract function values at LC points
 fLC = LC.testfun2D(xLC,yLC,nofun)
@@ -34,10 +34,13 @@ G = LC.LC2DdatM(m,kappa,fLC,wLC)
 C = LC.LC2Dcfsfft(m,kappa,G,average)
 
 #Evaluate the interpolation polynomial at new grid
-[x, y] = np.meshgrid(np.linspace(LCrange[0],LCrange[1],Nd),np.linspace(LCrange[2],LCrange[3],Nd))   
+[x,y] = np.meshgrid(np.linspace(LCrange[0],LCrange[1],Nd),np.linspace(LCrange[2],LCrange[3],Nd))   
 Sflin = LC.LC2Deval(C,m,x.flatten(),y.flatten(),LCrange)   
 SfLC  = LC.LC2Deval(C,m,xLC,yLC,LCrange)      
 Sf    = np.reshape(Sflin,(Nd,Nd))  
+
+#Evaluation of integral
+Qf    = LC.LC2Dquad(C,m,LCrange);
 
 # Calculate the errors
 maxerror   = LA.norm(Sflin-LC.testfun2D(x.flatten(),y.flatten(),nofun),np.inf);
@@ -56,11 +59,12 @@ else:
 print('Number of interpolation points   : %23d' % NoLC)
 print('Maximal error for approximation  : %23.18f' % maxerror)
 print('Maximal error at LC points       : %23.18f \n' % maxerrorLC)
+print('Integral of function over range  : %23.18f \n' % Qf)
  
 fig = plt.figure(figsize=(10, 8))
 ax = plt.axes(projection='3d')
 ax.plot_surface(x, y, Sf, rstride=1, cstride=1,
                 cmap='Wistia', edgecolor='none',zorder = 1)
 ax.scatter(xLC, yLC, fLC, color = (0.8,0.8,0.8), edgecolor='k', linewidth=1, s = 50, zorder = 2)
-ax.set_title('Interpolant')
+ax.set_title('Polynomial interpolant on LC nodes',y=1.1)
 ax.view_init(50, -120)
